1830ad38bd7a06ced663a866172272173bdb935e
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-09-08 12:54:54 -0300

    logout confirmation

--- rfhayashi/xmonad.hs
+++ rfhayashi/xmonad.hs
@@ -1,12 +1,15 @@
 import XMonad
+import System.Exit (exitSuccess)
 
 import XMonad.Util.EZConfig
+import XMonad.Prompt.ConfirmPrompt
 
 ulauncher = spawn "ulauncher"
 
 main = xmonad $ def
          `additionalKeysP`
          [ ("M-S-q", kill)
+         , ("M-S-l", confirmPrompt def "logout" $ io exitSuccess)
          , ("M-p", ulauncher)
          , ("M-<Space>", ulauncher)
          ]
