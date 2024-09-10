75d63a9a5eb8320dc73f2f83d6d99e488ea9a356
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-09-10 14:04:45 -0300

    Revert "stylix"
    This reverts commit 78ff1f645cb08f7b75a70f4218fd477db20f70a7.

--- configuration.nix
+++ configuration.nix
@@ -80,12 +80,4 @@
 
   fonts.packages = with pkgs; [ source-code-pro ];
 
-  stylix = {
-    enable = true;
-    image = pkgs.fetchurl {
-      url = "https://fedoraproject.org/w/uploads/0/01/F8-wallpaper-morning.png";
-      sha256 = "sha256-UPn9OhXEUHxedWx5+Uz6QzAVverPM+oiUMhhb0VBR0c=";
-    };
-  };
-
 }
--- flake.nix
+++ flake.nix
@@ -5,16 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix/release-24.05";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        stylix.nixosModules.stylix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
