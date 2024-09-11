a9b2fb348b6b5d8ee5458062ccc8f5824b407af2
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-09-11 13:22:14 -0300

    terminal + firefox key bindings

--- rfhayashi/xmonad.hs
+++ rfhayashi/xmonad.hs
@@ -24,7 +24,9 @@ main = xmonad $ def
           ,("M-<Space>", ulauncher)
           ,("M-<Escape>", spawn "xlock -mode blank")
           ,("M-S-<Backspace>", spawn "switch-keyboard-layout")
-          ,("M-s", spawn "cd ~/Downloads && scrot --select")]
+          ,("M-s", spawn "cd ~/Downloads && scrot --select")
+          ,("M-S-<Return>", spawn "firefox")
+          ,("M-<Return>", spawn "xterm")]
           ++ switchWorkspaceKeys)
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
