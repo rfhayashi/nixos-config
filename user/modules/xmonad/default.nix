5374c601d768bdadf6b42ca7ed8c3a3a7301ff4e
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-09-24 14:20:50 -0300

    move switch-keyboard-layout to xmonad module

--- user/modules/xmonad/default.nix
+++ user/modules/xmonad/default.nix
@@ -2,6 +2,16 @@
 with lib;
 let
   cfg = config.rfhayashi.xmonad;
+
+  switch-keyboard-layout = pkgs.writeScriptBin "switch-keyboard-layout" ''
+    #!${pkgs.babashka}/bin/bb
+
+    (let [output (:out (shell/sh "${pkgs.xorg.setxkbmap}/bin/setxkbmap" "-query"))
+          [_ layout] (re-find #"layout: *(\w+)\n" output)
+          new-layout (case layout "br" "us" "br")]
+      (shell/sh "${pkgs.xorg.setxkbmap}/bin/setxkbmap" new-layout))
+  '';
+
 in
 {
   options.rfhayashi.xmonad = {
@@ -23,6 +33,7 @@ in
       alsa-utils
       alacritty
       blueman
+      switch-keyboard-layout
     ];
 
     xsession.windowManager.xmonad = {
