d59bec042cf96bcf0a1470aa3dd02b12818ce378
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-28 11:20:30 -0300

    git ssh sign key

--- user/modules/git/default.nix
+++ user/modules/git/default.nix
@@ -3,6 +3,7 @@
   programs.gh.enable = true;
 
   sops.secrets."git/ssh_key" = {};
+  sops.secrets."git/sign_ssh_key" = {};
 
   programs.ssh = {
     enable = true;
@@ -19,9 +20,12 @@
     enable = true;
     userName = metadata.fullname;
     userEmail = metadata.email;
+    extraConfig = {
+      gpg.format = "ssh";
+    };
     signing = {
       signByDefault = true;
-      key = metadata.email;
+      key = config.sops.secrets."git/sign_ssh_key".path;
     };
   };
 
