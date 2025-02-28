399399c1a6e8f29683374b12af6964b12d3b25b3
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-02-28 16:20:09 -0300

    simplify desktop items

--- user/config/desktop-items/default.nix
+++ user/config/desktop-items/default.nix
@@ -1,27 +1,28 @@
 { pkgs, local-pkgs, ...}:
+let
+  confirm = (name: verb: command:
+    pkgs.writeShellScript name ''
+      ${pkgs.i3}/bin/i3-nagbar -t warning -m 'Do you really want to ${verb}?' -B 'Yes, ${verb}' '${command}'
+    ''
+  );
+in
 {
   rfhayashi.desktopItems = [
     {
       name = "poweroff";
-      exec = pkgs.writeShellScript "poweroff" ''
-        ${pkgs.i3}/bin/i3-nagbar -t warning -m 'Do you really want to power off?' -B 'Yes, power off' 'poweroff'  
-      '';
+      exec = confirm "poweroff" "power off" "poweroff";
       desktopName = "Power off";
       icon = "${./icons/system-shutdown-svgrepo-com.svg}";
     }
     {
       name = "reboot";
-      exec = pkgs.writeShellScript "reboot" ''
-        ${pkgs.i3}/bin/i3-nagbar -t warning -m 'Do you really want to reboot?' -B 'Yes, reboot' 'reboot'
-      '';
+      exec = confirm "reboot" "reboot" "reboot";
       desktopName = "Reboot";
       icon = "${./icons/system-reboot-svgrepo-com.svg}";
     }
     {
       name = "logout";
-      exec = pkgs.writeShellScript "logout" ''
-        ${pkgs.i3}/bin/i3-nagbar -t warning -m 'Do you really want to logout?' -B 'Yes, logout' 'logout'
-      '';
+      exec = confirm "logout" "logout" "logout";
       desktopName = "Logout";
       icon = "${./icons/logout-svgrepo-com.svg}";
     }
