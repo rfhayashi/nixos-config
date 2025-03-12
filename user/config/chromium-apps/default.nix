d7ab96251fc4e1ad1b89be49f4c816d05239f167
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-03-12 17:47:23 -0300

    youtube-music chromium app

--- user/config/chromium-apps/default.nix
+++ user/config/chromium-apps/default.nix
@@ -6,5 +6,10 @@
       url = "https://app.todoist.com";
       icon = ./icons/todoist-icon-svg-repo-com.svg;
     };
+    youtube-music = {
+      desktopName = "Youtube Music";
+      url = "https://music.youtube.com";
+      icon = ./icons/youtube-music-svg-repo-com.svg;
+    };
   };
 }
