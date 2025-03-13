cf3e5543b86980ecdf725830eaf9441854366afb
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-03-13 14:31:34 -0300

    experiment with running portal as a flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,13 @@
     sops-nix.url = "github:Mic92/sops-nix";
     devshell.url = "github:rfhayashi/devshells?dir=devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
+    portal = {
+      url = "github:djblue/portal";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, sops-nix, home-manager, devshell, ... }@inputs:
+  outputs = { self, nixpkgs, sops-nix, home-manager, devshell, portal, ... }@inputs:
     let
       system = "x86_64-linux";
       base-metadata = (nixpkgs.lib.modules.importTOML ./metadata.toml).config;
@@ -18,22 +22,21 @@
       local-pkgs = (import ./packages) { pkgs = import nixpkgs { inherit system; }; };
       devshell-pkgs = devshell.packages.${system};
     in
-    {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./system
-        sops-nix.nixosModules.sops
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${metadata.username} = import ./user;
-          home-manager.extraSpecialArgs = { inherit metadata local-pkgs devshell-pkgs; };
-        }
-      ];
-      specialArgs = { inherit metadata local-pkgs; };
-    };
-  };
+      {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./system
+            sops-nix.nixosModules.sops
+            home-manager.nixosModules.home-manager {
+              home-manager.sharedModules = [ inputs.sops-nix.homeManagerModules.sops ];
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${metadata.username} = import ./user;
+              home-manager.extraSpecialArgs = { inherit metadata local-pkgs devshell-pkgs portal; };
+            }
+          ];
+          specialArgs = { inherit metadata local-pkgs; };
+        };
+      };
 }
