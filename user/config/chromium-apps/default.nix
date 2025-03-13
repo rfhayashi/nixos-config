2e54eb6c3cbee6f6a31a936dad8346eb89fc215f
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-03-13 12:53:25 -0300

    integrate chromium apps with scratchpad

--- user/config/chromium-apps/default.nix
+++ user/config/chromium-apps/default.nix
@@ -3,17 +3,17 @@
   rfhayashi.chromiumApps = {
     chatgpt = {
       desktopName = "Chat GPT";
-      url = "https://chatgpt.com";
+      appHost = "chatgpt.com";
       icon = ./icons/chatgpt-icon.svg;
     };
     todoist = {
       desktopName = "Todoist";
-      url = "https://app.todoist.com";
+      appHost = "app.todoist.com";
       icon = ./icons/todoist-icon-svg-repo-com.svg;
     };
     youtube-music = {
       desktopName = "Youtube Music";
-      url = "https://music.youtube.com";
+      appHost = "music.youtube.com";
       icon = ./icons/youtube-music-svg-repo-com.svg;
     };
   };
--- user/modules/chromium-apps/default.nix
+++ user/modules/chromium-apps/default.nix
@@ -2,9 +2,31 @@
 let
   inherit (lib) mkOption types;
   cfg = config.rfhayashi;
+  #  wmctrl -l -x | mlr --ojson --ifs-regex="\s+" put '$id=$1; $desktop=$2; $class=$3' then cut -f id,desktop,class
+  startOrScratchpad = (instance: executable: pkgs.writeScript "start-or-scratchpad" ''
+    #!${pkgs.babashka}/bin/bb
+    (require '[babashka.process :as process])
+
+    (let [[instance executable] ["${instance}" "${executable}"]]
+      (let [win-output (:out (process/shell {:out :string} "wmctrl -l -x"))
+            win-lines (str/split-lines win-output)
+            win-data (fn [line]
+                       (let [[id desktop class] (str/split line #"\s+")]
+                         {:id id :desktop desktop :class class}))
+            windows (map win-data win-lines)
+            window? (fn [{:keys [class]}]
+                      (and (re-find (re-pattern instance) class)
+                           (re-find #"Chromium-browser" class)))
+            [{:keys [id desktop] :as win}] (filter window? windows)]
+        (if win
+          (if (= desktop = "-1")
+            (process/shell "${pkgs.i3}/bin/i3-msg" (format "[id=\"%s\"] scratchpad show" id))
+            (process/shell "${pkgs.i3}/bin/i3-msg" (format "[id=\"%s\"] focus" id)))
+          (process/shell executable))))
+  '');
   chromiumApp = types.submodule {
     options = {
-      url = mkOption {
+      appHost = mkOption {
         type = types.str;
       };
       desktopName = mkOption {
@@ -15,18 +37,18 @@ let
       };
     };
   };
-  appPackages = (id : { url, desktopName, icon, ... }:
+  appPackage = (id : { appHost, desktopName, icon, ... }:
     let
-      package = pkgs.writeShellScriptBin id ''
-       ${pkgs.chromium}/bin/chromium --app="${url}"
+      executable = pkgs.writeShellScript id ''
+       ${pkgs.chromium}/bin/chromium --app="https://${appHost}"
       '';
+      exec = startOrScratchpad appHost executable;
       desktopItem = pkgs.makeDesktopItem {
         name = id;
-        exec = "${package}/bin/${id}";
-        inherit desktopName icon;
+        inherit exec desktopName icon;
       };
     in
-      [ package desktopItem ]);
+      desktopItem);
 in
 {
   options.rfhayashi.chromiumApps = mkOption {
@@ -35,6 +57,6 @@ in
   };
 
   config = {
-    home.packages = lib.flatten (lib.mapAttrsToList appPackages cfg.chromiumApps);
+    home.packages = (lib.mapAttrsToList appPackage cfg.chromiumApps);
   };
 }
