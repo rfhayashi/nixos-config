1f0782288e42b55a7a0a3e9d166c36bce946e806
Rui Fernando Hayashi <rfhayashi@gmail.com>
2026-04-15 15:01:20 -0300

    eca rewrite model

--- user/modules/eca/default.nix
+++ user/modules/eca/default.nix
@@ -2,6 +2,7 @@
   home.file.".config/eca/config.json".text = builtins.toJSON {
     defaultProvider = "github-copilot";
     defaultModel = "github-copilot/claude-opus-4.6";
+    rewrite.model = "github-copilot/claude-sonnet-4.6";
     plugins.install = [
       "caveman"
     ];
