501f9ee4bdcaf2498644c38933ffdd409c5de84a
Rui Fernando Hayashi <rfhayashi@gmail.com>
2026-07-06 14:55:38 -0300

    system update

--- system/default.nix
+++ system/default.nix
@@ -85,9 +85,6 @@
   };
 
   virtualisation.docker.enable = true;
-  virtualisation.podman.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ]; # required by virtualbox
 
   system.stateVersion = "24.05";
 
